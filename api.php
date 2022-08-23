711f6b0d23394041c9b73bc4b9f2d310cdf14794
oluwadamilola <dammyolukoju@gmail.com>
2022-08-23 22:18:49 +0100

    implement auth api

--- api.php
+++ api.php
@@ -1,5 +1,7 @@
 <?php
 
+$base_url = "https://aohs1.trudoc24x7.info";
+
 $url = "https://aohs1.trudoc24x7.info/api/oauth/token";
 
 $curl = curl_init($url);
@@ -32,5 +34,42 @@ $resp = curl_exec($curl);
 curl_close($curl);
 var_dump($resp);
 
+$json_resp = json_decode($resp);
+$access_token = $json_resp->access_token;
+
+
+$user_url = "https://aohs1.trudoc24x7.info/api/v1/census/addSinglePatient";
+$data = array(
+  "first_name"=> "Test",
+    "last_name"=>"Dami",
+    "gender"=> "female",
+    "date_of_birth"=>"1992-05-19",
+    "country_id"=> "1",
+    "city_id"=> "1",
+    "national_id"=> "11111111111",
+    "nationality_id"=>"1",
+    "mobile"=>"07038240033",
+    "company_code"=>"EkoTeleMed"
+
+);
+$headers = array(
+        'http' => array(
+        'header'  => "Content-type: application/json\r\n",
+        'method'  => 'POST',
+        'Authorization' => 'Bearer '.$access_token,
+        'content' => json_encode($data)
+    )
+);
+
+$context  = stream_context_create($options);
+$result = file_get_contents( $url, false, $context );
+$response = json_decode( $result );
+
+
+
+
+
 ?>
+
+
 
